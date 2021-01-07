d1619fb9d719822d2cec92c27b94c12278582daf
Me <mergorgec@gmail.com>
2021-01-07 13:38:33 +0100

    add useState

--- src/App.js
+++ src/App.js
@@ -20,19 +20,21 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' onChange={handleChange} />
-
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
       <hr />
-
       <List list={stories} />
     </div>
   );
